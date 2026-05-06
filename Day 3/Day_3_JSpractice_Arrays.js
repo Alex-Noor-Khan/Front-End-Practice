39b5039de359669260394540a8b90994d6d4c2d3
Alex Noor Khan <alexkhan456@gmail.com>
2026-05-07 00:46:45 +0600

    Add array practice file for Day 3

--- Day 3/Day_3_JSpractice_Arrays.js
+++ Day 3/Day_3_JSpractice_Arrays.js
@@ -87,7 +87,91 @@ const myChildren = array1.concat(array2, array3);
 document.getElementById("demo").innerHTML = myChildren;
 
 
+const points = [40, 100, 1, 5, 25, 10];
+document.getElementById("demo").innerHTML = points;  
 
+points.sort(function(a, b){return a - b});
+document.getElementById("demo2").innerHTML = points;
+
+
+
+// const points = [40, 100, 1, 5, 25, 10];
+// document.getElementById("demo").innerHTML = myArrayMax(points);
+
+// function myArrayMax(arr) {
+//   return Math.max.apply(null, arr);
+// }
+
+
+const cars = [
+  {type:"Volvo", year:2016},
+  {type:"Saab", year:2001},
+  {type:"BMW", year:2010}
+];
+
+displayCars();
+
+cars.sort(function(a, b){return a.year - b.year});
+displayCars();
+
+function displayCars() {
+  document.getElementById("demo").innerHTML =
+  cars[0].type + " " + cars[0].year + "<br>" +
+  cars[1].type + " " + cars[1].year + "<br>" +
+  cars[2].type + " " + cars[2].year;
+}
+
+
+//Iteration
+
+const numbers = [45, 4, 9, 16, 25];
+
+let txt = "";
+numbers.forEach(myFunction);
+document.getElementById("demo").innerHTML = txt;
+
+function myFunction(value, index, array) {
+  txt += value + "<br>"; 
+}
+
+
+// It only runs the function on elements that actually exist.
+// If an array has "empty slots" (also called holes), map() will skip them instead of treating them as undefined
+
+const numbers1 = [45, 4, 9, 16, 25];
+const numbers2 = numbers1.map(myFunction);
+
+document.getElementById("demo").innerHTML = numbers2;
+
+function myFunction(value, index, array) {
+  return value * 2;
+}
+
+c
+
+//if else checker
+const numbers1 = [45, 4, 9, 16, 25];
+const numbers2 = numbers1.map(myFunction);
+
+document.getElementById("demo").innerHTML = numbers2;
+
+function myFunction(value) {
+  return value * 2;
+}
+
+
+
+// reduce() takes an array and reduces it to a single value by applying a function repeatedly.
+// It works by carrying an accumulator (a running total or result) and combining it with each element of the array.
+// You can think of it as folding the array into one outcome.
+const numbers = [45, 4, 9, 16, 25];
+let sum = numbers.reduce(myFunction);
+
+document.getElementById("demo").innerHTML = "The sum is " + sum;
+
+function myFunction(total, value) {
+  return total * value;
+}
 
 
 
